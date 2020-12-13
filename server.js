aa12ad75c4849afd720cabff94378e32041ecf20
jsreyl <jsreyl@unal.edu.co>
2020-12-12 20:49:50 -0500

    Cambios preparando para montar en Heroku

--- server.js
+++ server.js
@@ -36,13 +36,14 @@ app.use('/api', apiRouter);
 /*se debe contar un una ruta por medio de método post para el inicio de sesión de la siguiente manera:
 '/api/auth/signin'
 */
+app.set('PORT', process.env.PORT || 3000)
+
 app.get('/', function(req, res) {
     console.log("Estructura base del proyecto backend");
     res.send("Estructura base del proyecto backend");
 });
-const port = 3000
-app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`)
+app.listen(app.get('PORT'), () => {
+    console.log(`Running on http://localhost:${app.get('PORT')}`)
 })
 
 module.exports = app;

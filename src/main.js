b69238b3d3ba65ba61cbc247391aaf728b4fc1f0
jsreyl <jsreyl@unal.edu.co>
2020-12-12 21:21:29 -0500

    Changing paths to match challenge directions

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import router from './router'
 import axios from 'axios'
 
 const base = axios.create({
-  // baseURL : 'https://glacial-everglades-74306.herokuapp.com'
+  // baseURL : 'https://glacial-everglades-74306.herokuapp.com' //Professor
   // baseURL : 'http://localhost:3000/'
-  baseURL : 'https://peaceful-river-55991.herokuapp.com'
+  // baseURL : 'https://peaceful-river-55991.herokuapp.com' //Class
+  baseURL : 'https://glacial-cove-40185.herokuapp.com/' //Semana-3-18 repo
 })
 Vue.prototype.$http = base
 
